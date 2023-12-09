f6474095b95ade5d8c4157c39b6dfd29cacafc9c
AlvaroFenero <al.fenero@duocuc.cl>
2023-12-08 22:28:13 -0300

    agregado capacitor

--- src/app/pages/alumno/alumno.page.ts
+++ src/app/pages/alumno/alumno.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { AlertController, IonicModule, LoadingController } from '@ionic/angular';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserModel } from 'src/app/models/UserModel';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-alumno',
@@ -15,13 +16,18 @@ import { UserModel } from 'src/app/models/UserModel';
 export class AlumnoPage implements OnInit {
   alumnoInfoReceived: UserModel | undefined;
   idUserHtmlRouterLink: any;
+  barcodes: Barcode[] = [];
+  qrEscaneado: string = "";
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute ) { 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute,
+    private alertController: AlertController,
+    private loadingController: LoadingController, ) { 
     this.alumnoInfoReceived = this.router.getCurrentNavigation()?.extras.state?.['user'];
     // Si quiero obtener un valor por URL usando routerLink
     this.idUserHtmlRouterLink = this.activatedRoute.snapshot.params['id'];
     // Obteniendo el ID podria buscar en algún arreglo o BD el usuario con el id
     console.log("Valor obtenido desde URL: ",this.idUserHtmlRouterLink);
+
   }
 
   ngOnInit() {
@@ -29,5 +35,44 @@ export class AlumnoPage implements OnInit {
   volverinicio(){
     this.router.navigate(['/login']);
   }
+
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.qrEscaneado = barcodes[0].displayValue;
+    this.barcodes.push(...barcodes);
+    console.log('Barcode data', barcodes[0].displayValue);
+
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  public async openSettings(): Promise<void> {
+    await BarcodeScanner.openSettings();
+  }
+
+  public async installGoogleBarcodeScannerModule(): Promise<void> {
+    await BarcodeScanner.installGoogleBarcodeScannerModule();
+  }
+
+
+
+
 
 }
